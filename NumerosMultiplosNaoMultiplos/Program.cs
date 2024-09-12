0db2a09a7cdf2b032c6b484461419c3343e87ff9
Mikael de Jesus <mikaeljl824@gmail.com>
2024-09-12 20:01:39 -0300

    Adicionar arquivos de projeto.

---
+++ NumerosMultiplosNaoMultiplos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumerosMultiplosNaoMultiplos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+
+            int n1 = int.Parse(valores[0]);
+            int n2 = int.Parse(valores[1]);
+
+            if (n1 % n2 == 0)
+            {
+                Console.WriteLine("SAO MULTIPLOS");
+
+            }
+            else { Console.WriteLine("Não SAO MULTIPLOS"); }
+
+        }
+    }
+}
